4c1df50d17e23397e128d1da0567cafc4db4d3eb
orangelife7 <orangelife7@op.pl>
2025-11-18 10:57:42 +0100

    17 adding physical book controller

---
+++ demo/src/main/java/com/example/demo/controller/PhysicalBookController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.PhysicalBook;
+import com.example.demo.service.CrudService;
+import com.example.demo.service.PhysicalBookService;
+
+@RestController
+@RequestMapping("/physicalBook")
+public class PhysicalBookController extends CrudController<PhysicalBook> {
+
+	@Autowired
+	private PhysicalBookService physicalBookService;
+	
+	public CrudService<PhysicalBook> getService() {
+		return physicalBookService;
+	}
+}
