94f4902e2494c1665aa0e9f9d4ef5f3b6d9ad22d
orangelife7 <orangelife7@op.pl>
2025-11-19 12:28:10 +0100

    29 adding tests to OrderAmountToPayTest and refactoring

--- demo/src/test/java/com/example/demo/OrderAmountToPayTest.java
+++ demo/src/test/java/com/example/demo/OrderAmountToPayTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,25 +12,87 @@ import com.example.demo.entity.Order;
 
 public class OrderAmountToPayTest {
 
+	@Test
+	public void testAmountToPayWhenEverythingOk() {
+		LocalDateTime loanDate = LocalDateTime.of(2025, 04, 05, 0, 0);
+		LocalDateTime returnDate = LocalDateTime.of(2025, 06, 12, 0, 0);
+		Order order = get(loanDate, returnDate, false);
+		assertEqualsAmount(order, BigDecimal.ZERO);
+	}
+
+	@Test
+	public void testAmountToPayWhenUndamagedAfterDeadline() {
+		LocalDateTime loanDate = LocalDateTime.of(2025, 05, 21, 17, 30);
+		LocalDateTime returnDate = LocalDateTime.of(2025, 9, 14, 12, 47);
+		Order order = get(loanDate, returnDate, false);
+		long subtractionDays = ChronoUnit.DAYS.between(order.getDeadline(), returnDate);
+		BigDecimal expected = BigDecimal.valueOf(subtractionDays);
+		assertEqualsAmount(order, expected);
+	}
+
+	@Test
+	public void testAmountToPayWhenUndamagedAfterMaximumDeadline() {
+		LocalDateTime loanDate = LocalDateTime.of(2025, 01, 15, 15, 0);
+		LocalDateTime returnDate = LocalDateTime.of(2025, 11, 19, 12, 15);
+		Order order = get(loanDate, returnDate, false);
+		long subDays1 = ChronoUnit.DAYS.between(order.getDeadline(), order.getMaximumDeadline());
+		BigDecimal expected1 = BigDecimal.valueOf(subDays1).multiply(BigDecimal.ONE);
+		long subtDays2 = ChronoUnit.DAYS.between(order.getMaximumDeadline(), returnDate);
+		BigDecimal expected2 = BigDecimal.valueOf(subtDays2).multiply(BigDecimal.TWO);
+		BigDecimal expected = expected1.add(expected2);
+		assertEqualsAmount(order, expected);
+	}
+
+	@Test
+	public void testAmountToPayWhenDamagedAndReturnedOnTime() {
+		LocalDateTime loanDate = LocalDateTime.of(2025, 07, 16, 0, 0);
+		LocalDateTime returnDate = LocalDateTime.of(2025, 9, 27, 13, 35);
+		Order order = get(loanDate, returnDate, true);
+		long subtraction = ChronoUnit.DAYS.between(loanDate, returnDate);
+		BigDecimal exp = BigDecimal.valueOf(subtraction).multiply(BigDecimal.ZERO);
+		BigDecimal expected = exp.add(BigDecimal.valueOf(50));
+		assertEqualsAmount(order, expected);
+	}
+
+	@Test
+	public void testAmountToPayWhenDamagedAndReturnedAfterDeadline() {
+		LocalDateTime loanDate = LocalDateTime.of(2025, 06, 04, 13, 57);
+		LocalDateTime returnDate = LocalDateTime.of(2025, 10, 03, 14, 32);
+		Order order = get(loanDate, returnDate, true);
+		long subtraction = ChronoUnit.DAYS.between(order.getDeadline(), returnDate);
+		BigDecimal exp = BigDecimal.valueOf(subtraction).multiply(BigDecimal.ONE);
+		BigDecimal expected = exp.add(BigDecimal.valueOf(50));
+		assertEqualsAmount(order, expected);
+	}
+
+	@Test
+	public void testAmountToPayWhenDamagedAndReturnAfterMaximumDeadline() {
+		LocalDateTime loanDate = LocalDateTime.of(2025, 04, 24, 15, 16);
+		LocalDateTime returnDate = LocalDateTime.of(2026, 02, 25, 9, 46);
+		Order order = get(loanDate, returnDate, true);
+
+		LocalDateTime maximumDeadline = order.getMaximumDeadline();
+		long subtraction1 = ChronoUnit.DAYS.between(order.getDeadline(), maximumDeadline);
+		BigDecimal exp1 = BigDecimal.valueOf(subtraction1).multiply(BigDecimal.ONE);
+		long subtraction2 = ChronoUnit.DAYS.between(maximumDeadline, returnDate);
+		BigDecimal exp2 = BigDecimal.valueOf(subtraction2).multiply(BigDecimal.TWO);
+		BigDecimal expected = exp1.add(exp2).add(BigDecimal.valueOf(50));
+		assertEqualsAmount(order, expected);
+	}
+
+	private Order get(LocalDateTime loanDate, LocalDateTime returnDate, boolean damaged) {
+		Order order = new Order();
+		order.setLoanDate(loanDate);
+		order.refreshDeadline();
+		order.refreshMaximumDeadline();
+		order.setReturnDate(returnDate);
+		order.setDamaged(damaged);
+		return order;
+	}
+
 	private void assertEqualsAmount(Order order, BigDecimal expectedAmount) {
 		order.refreshAmountToPay();
 		assertEquals(expectedAmount, order.getAmountToPay());
 	}
-
-	@Test
-	public void testAmountToPayWhenEverythingOk() {
-		Order order = new Order();
-		LocalDateTime loanDate = LocalDateTime.of(2025, 04, 05, 0, 0);
-		order.setLoanDate(loanDate);
-		LocalDateTime returnDate = LocalDateTime.of(2025, 06, 12, 0, 0);
-		order.setReturnDate(returnDate);
-		order.setDamaged(false);
-		assertEqualsAmount(order, BigDecimal.ZERO);
-
-	}
-	
-	
-	
-	
 
 }
