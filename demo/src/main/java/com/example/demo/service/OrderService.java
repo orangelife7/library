fd26892085466d11b4bd2060eb4a390f2aa99352
orangelife7 <orangelife7@op.pl>
2025-11-19 15:21:16 +0100

    32 RefreshStatus method to OrderService

--- demo/src/main/java/com/example/demo/service/OrderService.java
+++ demo/src/main/java/com/example/demo/service/OrderService.java
@@ -1,13 +1,17 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.Order;
 import com.example.demo.repository.CoreRepository;
 import com.example.demo.repository.OrderRepository;
 
 @Service
+@Transactional
 public class OrderService extends CrudService<Order> {
 
 	@Autowired
@@ -15,5 +19,13 @@ public class OrderService extends CrudService<Order> {
 
 	public CoreRepository<Order, Long> getRepository() {
 		return orderRepository;
+	}
+
+	public void refreshStatus() {
+		List<Order> orders = orderRepository.findAll();
+		
+		for (Order order : orders) {
+			order.refreshStatus();
+		}
 	}
 }
