efacbcc9e474ee94082e812b102db4d2df7374a0
orangelife7 <orangelife7@op.pl>
2025-12-11 16:28:41 +0100

    43 book-list component - downloading data in interval

--- demo/src/main/resources/static/js/components/book-list/book-list.component.js
+++ demo/src/main/resources/static/js/components/book-list/book-list.component.js
@@ -1,17 +1,26 @@
 app.component('bookList', {
 	templateUrl: '/js/components/book-list/book-list.component.html',
-	controller: function($http) {
+	controller: function($http, $interval) {
 		const ctrl = this;
 		
+		ctrl.$onInit = function() {
+			ctrl.load();
+			
+			$interval(function() {
+                ctrl.load();
+            }, 5000);
+		}
+		
 		ctrl.load = function() {
-		$http.get('http://localhost:8080/api/book/list')
-		.then(function(response) {
-			console.log(response.data);
-			ctrl.books = response.data;
-		})
-		.catch(function(error) {
-				console.error('Blad: ' + error);
+			$http.get('http://localhost:8080/api/book/list')
+			.then(function(response) {
+				console.log(response.data);
+				ctrl.books = response.data;
 			})
+			.catch(function(error) {
+					console.error('Blad: ' + error);
+				})
 		};
+		
 	}
 });
