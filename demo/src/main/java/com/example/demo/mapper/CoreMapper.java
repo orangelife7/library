70a86c22a8696f89bbff9a53afb2bd6887a8d9aa
orangelife7 <orangelife7@op.pl>
2026-01-20 21:39:10 +0100

    69 ObjectMapper - LocalDateTime serialization config

--- demo/src/main/java/com/example/demo/mapper/CoreMapper.java
+++ demo/src/main/java/com/example/demo/mapper/CoreMapper.java
@@ -1,14 +1,27 @@
 package com.example.demo.mapper;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 public class CoreMapper extends ObjectMapper {
 
 	private SimpleFilterProvider provider;
 
 	{
+		JavaTimeModule javaTimeModule = new JavaTimeModule();
+		javaTimeModule.addSerializer(LocalDateTime.class,
+				new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+
+		registerModule(javaTimeModule);
+		disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
 		provider = new SimpleFilterProvider();
 		setFilterProvider(provider);
 	}
