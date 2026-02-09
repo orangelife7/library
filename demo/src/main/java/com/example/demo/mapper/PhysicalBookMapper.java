b6e417cd7bdefec6a635d32ff1c99392c3d6c3cb
orangelife7 <orangelife7@op.pl>
2026-02-09 15:56:59 +0100

    116 Quick fix - Metamodel in ObjectMapper and style css

--- demo/src/main/java/com/example/demo/mapper/PhysicalBookMapper.java
+++ demo/src/main/java/com/example/demo/mapper/PhysicalBookMapper.java
@@ -12,7 +12,7 @@ public class PhysicalBookMapper extends CoreMapper {
 	{
 		addFilter("physicalBookFilter", filterOutAllExcept(PhysicalBook_.ID, PhysicalBook_.CATALOG_NUMBER, PhysicalBook_.DESCRIPTION, PhysicalBook_.BOOK, PhysicalBook_.ORDERS));
 		
-		addFilter("bookFilter", filterOutAllExcept(Book_.ID, Book_.TITLE, Book_.TITLE, "label"));
+		addFilter("bookFilter", filterOutAllExcept(Book_.ID, Book_.TITLE, Book_.AUTHOR, "label"));
 		
 		addFilter("orderFilter", filterOutAllExcept(Order_.ID, Order_.LOAN_DATE, Order_.DEADLINE, Order_.MAXIMUM_DEADLINE, 
 				Order_.RETURN_DATE, Order_.CANCELLED, Order_.PREPARED, Order_.DAMAGED, Order_.PAID, Order_.AMOUNT_TO_PAY, Order_.STATUS));
