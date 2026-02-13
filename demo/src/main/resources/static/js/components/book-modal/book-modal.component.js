07cb2fbd501a5393a63303fbfc093d4378dd232d
orangelife7 <orangelife7@op.pl>
2026-02-13 14:38:00 +0100

    133 Refactoring - entity lists in modals

--- demo/src/main/resources/static/js/components/book-modal/book-modal.component.js
+++ demo/src/main/resources/static/js/components/book-modal/book-modal.component.js
@@ -4,20 +4,4 @@ app.component('bookModal', {
 	close: '&'	
 	 },
 	 templateUrl: getComponentPath('book-modal'),
-	 controller: function(HttpService, $controller, $scope) {
-	 		const vm = this;
-	 		
-	 		$controller('ListController', 
-	 				{ 
-	 					$scope: $scope 
-	 				});
-	 				
-	 		    $scope.init("book");
-	 		
-	 			vm.selectedBookId = null;
-
-	 			vm.selectExisting = function() {
-	 			    vm.resolve.select(vm.selectedBookId);
-	 		};
-	 	}
 });
---
+++ demo/src/main/resources/static/js/components/common-select-modal/common-select-modal.component.js
@@ -0,0 +1,25 @@
+app.component('commonSelectModal', {
+	bindings: {
+		entityLabel: '@',
+		entityUrl: '@',
+		resolve: '<',
+		optionLabel: '&'
+	},
+	transclude: true,
+	templateUrl: getComponentPath('common-select-modal'),
+	
+	controller: function($controller, $scope) {
+		const vm = this;
+		
+		vm.$onInit = function() {
+			$controller('ListController', { $scope: $scope });
+		 	$scope.init(vm.entityUrl);
+			vm.selectedId = null;
+					
+			vm.selectExisting = function() {
+				vm.resolve.select(vm.selectedId);
+			};
+		}
+		
+	}
+});
