ffcd5c31edd82710b7bed7753fcdde0d03babf10
orangelife7 <orangelife7@op.pl>
2026-02-16 09:56:24 +0100

    135 Entity lists in modals - customer and employee

--- demo/src/main/resources/static/js/components/customer-modal/customer-modal.component.js
+++ demo/src/main/resources/static/js/components/customer-modal/customer-modal.component.js
@@ -4,20 +4,4 @@ app.component('customerModal', {
 	close: '&'	
 	 },
 	 templateUrl: getComponentPath('customer-modal'),
-	 controller: function(HttpService, $controller, $scope) {
-		const vm = this;
-		
-		$controller('ListController', 
-				{ 
-					$scope: $scope 
-				});
-				
-		    $scope.init("customer");
-		
-			vm.selectedCustomerId = null;
-
-			vm.selectExisting = function() {
-			    vm.resolve.select(vm.selectedCustomerId);
-		};
-	}
 });
--- demo/src/main/resources/static/js/components/employee-modal/employee-modal.component.js
+++ demo/src/main/resources/static/js/components/employee-modal/employee-modal.component.js
@@ -4,20 +4,4 @@ app.component('employeeModal', {
 	close: '&'	
 	 },
 	 templateUrl: getComponentPath('employee-modal'),
-	 controller: function(HttpService, $controller, $scope) {
-	 		const vm = this;
-	 		
-	 		$controller('ListController', 
-	 				{ 
-	 					$scope: $scope 
-	 				});
-	 				
-	 		    $scope.init("employee");
-	 		
-	 			vm.selectedEmployeeId = null;
-
-	 			vm.selectExisting = function() {
-	 			    vm.resolve.select(vm.selectedEmployeeId);
-	 		};
-	 	}
 });
