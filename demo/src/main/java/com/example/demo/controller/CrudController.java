3d44471820a8f57e451c067d4e5aaa368350849e
orangelife7 <orangelife7@op.pl>
2026-02-19 10:16:09 +0100

    140 Endpoint downloading the list - pagination params

--- demo/src/main/java/com/example/demo/controller/CrudController.java
+++ demo/src/main/java/com/example/demo/controller/CrudController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.BaseEntity;
@@ -25,29 +26,26 @@ public abstract class CrudController<T extends BaseEntity> {
 	}
 
 	/*
-	@GetMapping("/list")
-	public List<T> findAll() {
-		return getService().findAll();
-	}
-	*/
+	 * @GetMapping("/list") public List<T> findAll() { return
+	 * getService().findAll(); }
+	 */
 
 	@GetMapping("/list")
-	public String listJson() throws JsonProcessingException {
+	public String listJson(@RequestParam int page, @RequestParam int size, @RequestParam String sort)
+			throws JsonProcessingException {
 		return getService().getListJson();
 	}
-	
+
 	@GetMapping("/{id}")
 	public String detailsJson(@PathVariable Long id) throws JsonProcessingException {
 		return getService().getDetailsJson(id);
 	}
 
 	/*
-	@GetMapping("/{id}")
-	public T findById(@PathVariable Long id) {
-		return getService().findById(id)
-				.orElseThrow(() -> new EntityNotFoundException("Entity not found with id: " + id));
-	}
-	*/
+	 * @GetMapping("/{id}") public T findById(@PathVariable Long id) { return
+	 * getService().findById(id) .orElseThrow(() -> new
+	 * EntityNotFoundException("Entity not found with id: " + id)); }
+	 */
 
 	@PostMapping("/{id}/update")
 	public void update(@PathVariable Long id, @RequestBody Map<String, String> payload) {
@@ -58,37 +56,36 @@ public abstract class CrudController<T extends BaseEntity> {
 	public void delete(@PathVariable Long id) {
 		getService().delete(id);
 	}
-	
+
 	@GetMapping("/{fieldName}/type")
 	public ResponseEntity<String> fieldType(@PathVariable String fieldName) {
 
-	    String fieldType = getFieldType(fieldName);
-	    
-	    return ResponseEntity.ok(String.format( "{\"data\" : \"%s\"}", fieldType));
+		String fieldType = getFieldType(fieldName);
+
+		return ResponseEntity.ok(String.format("{\"data\" : \"%s\"}", fieldType));
 	}
-	
+
 	private Class<?> getEntityClass() {
-	    return (Class<?>) ((java.lang.reflect.ParameterizedType)
-	            getClass().getGenericSuperclass())
-	            .getActualTypeArguments()[0];
+		return (Class<?>) ((java.lang.reflect.ParameterizedType) getClass().getGenericSuperclass())
+				.getActualTypeArguments()[0];
 	}
-	
+
 	private String getFieldType(String fieldName) {
 		try {
-	        Class<?> entityClass = getEntityClass();
+			Class<?> entityClass = getEntityClass();
 
-	        Class<?> fieldType = entityClass.getDeclaredField(fieldName).getType();
+			Class<?> fieldType = entityClass.getDeclaredField(fieldName).getType();
 
-	        if (java.time.LocalDateTime.class.equals(fieldType)) {
-	            return "localDateTime";
-	        }
-	        if (boolean.class.equals(fieldType) || Boolean.class.equals(fieldType)) {
-	            return "boolean";
-	        }
-	    } catch (Exception ignored) {
-	        
-	    }
+			if (java.time.LocalDateTime.class.equals(fieldType)) {
+				return "localDateTime";
+			}
+			if (boolean.class.equals(fieldType) || Boolean.class.equals(fieldType)) {
+				return "boolean";
+			}
+		} catch (Exception ignored) {
 
-	    return "text";
+		}
+
+		return "text";
 	}
 }
